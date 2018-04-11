22bcd5d30bc82f1d2dc20bebd68eed462d373117
Rohan Chaudhari <rohan1chaudhari@gmail.com>
2018-04-11 12:47:35 +0200

    Upload first try

---
+++ src/main/scala/dsti/Streamer.scala
@@ -0,0 +1,42 @@
+package dsti
+
+import java.util.Calendar
+
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author ${user.name}
+ */
+object Streamer {
+
+  // Your code here
+  
+  def main(args : Array[String]) {
+    println("Hello World ! ")
+
+
+    //Config
+    val config = new SparkConf().setAppName("twitter-stream-sentiment")
+    val sc = new SparkContext(config)
+    sc.setLogLevel("WARN")
+    val ssc = new StreamingContext(sc, Seconds(5))
+    System.setProperty("twitter4j.oauth.consumerKey", "e4dhcvBgVanr1dEg0H85RPNSn")
+    System.setProperty("twitter4j.oauth.consumerSecret", "iax8ymF6PopnMxhxbESayIKeAkfbXbGQBdyAmki3IAGlTUZowJ")
+    System.setProperty("twitter4j.oauth.accessToken", "331813667-4p6698Qt0eM4dRjNSF4PxZST19dGz3grq52pPtkY")
+    System.setProperty("twitter4j.oauth.accessTokenSecret", "Oa7ooLmnyOUYQ0kTuHvD0oO8NEAvpKdSb1HpM46Ya6ZjV")
+    val stream = TwitterUtils.createStream(ssc, None,Array("strike","Strike"))
+
+
+    //stream.foreachRDD(rdd => println(rdd))
+    // Your code here
+
+    val data = stream.map {status => (status.getHashtagEntities.map(_.getText),status.getText(),status.getGeoLocation(),status.getPlace())  }
+    val tags = stream.flatMap(status => status.getHashtagEntities.map(_.getText))
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
